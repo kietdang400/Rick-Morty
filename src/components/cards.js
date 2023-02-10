a048a8c1795d3863b7db7d81bbded96c9d5b28e1
Kiet Dang <kdgdang@ucdavis.edu>
2023-02-09 18:28:09 -0800

    need to add desktop design

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,7 +6,7 @@ const card=(props)=>{
     return(
         <div className="card">
             <div className="grid-container">
-            <div className="grid-items"><label className="title">Type: </label>{props.type1}</div>
+            <div className="grid-items card1"><label className="title">Type: </label>{props.type1}</div>
             <div className="grid-items"><label className="title">Dimension: </label>{props.dimension1}</div>
             <div className="grid-items"><label className="title">Population: </label>{props.population}</div>
             </div>
--- src/components/character.js
+++ src/components/character.js
@@ -8,10 +8,10 @@ const Character=(props)=>{
         <div className='character'>
             <img src={props.image} alt={props.name}></img>
             <div className="info-container">
-                <label className="labels"><u>Name</u></label> <div className="info">{props.name}</div>
-                <label className="labels"><u>Species</u></label> <div className="info">{props.species}</div>
-                <label className="labels"><u>Gender</u></label> <div className="info">{props.gender}</div>
-                <label className="labels"><u>Status:</u></label> <div className="info">{props.status}</div>
+                <label className="labels">Name:</label> <div className="info">{props.name}</div>
+                <label className="labels">Species:</label> <div className="info">{props.species}</div>
+                <label className="labels">Gender:</label> <div className="info">{props.gender}</div>
+                <label className="labels">Status:</label> <div className="info">{props.status}</div>
             </div>
         </div>
     )
